47da81f7ef7e178d1cf76e1eeae3c9c7b85313e9
Willy Gomez <wp_gomez@hotmail.com>
2021-06-02 14:24:22 -0500

    Video 52

--- resources/views/livewire/add-cart-item-color.blade.php
+++ resources/views/livewire/add-cart-item-color.blade.php
@@ -3,6 +3,7 @@
 
     <select wire:model="color_id" class="form-control w-full">
         <option value="" selected disabled>Seleccionar un color</option>
+        
         @foreach ($colors as $color)
             <option value="{{$color->id}}">{{$color->name}}</option>
         @endforeach
--- resources/views/livewire/add-cart-item-size.blade.php
+++ resources/views/livewire/add-cart-item-size.blade.php
@@ -1,3 +1,25 @@
 <div>
-    {{-- Be like water. --}}
+    <div>
+        <p class="text-xl text-gray-700">Talla:</p>
+
+        <select wire:model="size_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione una talla</option>
+
+            @foreach ($sizes as $size)
+            <option value="{{$size->id}}">{{$size->name}}</option>
+            @endforeach
+        </select>
+    </div>
+
+    <div class="mt-2">
+        <p class="text-xl text-gray-700">Color:</p>
+
+        <select wire:model="color_id" class="form-control w-full">
+            <option value="" selected disabled>Seleccione un color</option>
+
+            @foreach ($colors as $color)
+            <option value="{{$color->id}}">{{$color->name}}</option>
+            @endforeach
+        </select>
+    </div>
 </div>
