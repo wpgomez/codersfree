5326ba004f92d69cd625c3b5de6352c7140e0160
Willy Gomez <wp_gomez@hotmail.com>
2021-06-03 14:48:06 -0500

    Video 69

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -9,13 +9,13 @@
         <div class="bg-white rounded-lg shadow mt-1">
             <div class="px-4 py-3 space-y-1">
                 @forelse ($products as $product)
-                    <div class="flex">
+                    <a href="{{ route('products.show', $product) }}" class="flex">
                         <img class="w-16 h-12 object-cover" src="{{ Storage::url($product->images->first()->url) }}" alt="">
                         <div class="ml-4 text-gray-700">
                             <p class="text-lg font-semibold leading-5">{{$product->name}}</p>
                             <p>Categoría: {{$product->subcategory->category->name}}</p>
                         </div>
-                    </div>
+                    </a>
                 @empty
                     <p class="text-lg leading-5">
                         No existe ningún registro con los parametros especificados
