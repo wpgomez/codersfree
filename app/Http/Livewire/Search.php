af170107c1f990941a05fc94724869913230d985
Willy Gomez <wp_gomez@hotmail.com>
2021-06-03 14:26:55 -0500

    Video 67

--- app/Http/Livewire/Search.php
+++ app/Http/Livewire/Search.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Product;
 use Livewire\Component;
 
 class Search extends Component
 {
+    public $search;
+
     public function render()
     {
-        return view('livewire.search');
+        $products = Product::where('name', 'LIKE','%' . $this->search . '%')->get();
+
+        return view('livewire.search', compact('products'));
     }
 }
--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,7 +1,26 @@
 <div class="flex-1 relative">
-    <x-jet-input type='text' class="w-full" placeholder="¿Estás buscando algún producto?" />
+    <x-jet-input wire:model="search" type='text' class="w-full" placeholder="¿Estás buscando algún producto?" />
 
     <button class="absolute top-0 right-0 w-12 h-full bg-orange-500 flex items-center justify-center rounded-r-md">
         <x-search size="35" color="white"/>
     </button>
+
+    <div class="absolute w-full">
+        <div class="bg-white rounded-lg shadow mt-1">
+            <div class="px-4 py-3 space-y-1">
+                @forelse ($products as $product)
+                    <div class="flex">
+                        <img class="w-16 h-12 object-cover" src="{{ Storage::url($product->images->first()->url) }}" alt="">
+
+                        <div class="ml-4 text-gray-700">
+                            <p class="text-lg font-semibold leading-5">{{$product->name}}</p>
+                            <p>Categoría: {{$product->subcategory->category->name}}</p>
+                        </div>
+                    </div>
+                @empty
+                    
+                @endforelse
+            </div>
+        </div>
+    </div>
 </div>
