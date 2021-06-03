b390c0992aa56664f15d2695defe934e039e602b
Willy Gomez <wp_gomez@hotmail.com>
2021-06-03 15:30:16 -0500

    Video 72

--- resources/views/livewire/dropdown-cart.blade.php
+++ resources/views/livewire/dropdown-cart.blade.php
@@ -48,7 +48,7 @@
                 <div class="py-2 px-3">
                     <p class="text-lg text-gray-700 mt-2 mb-3"><span class="font-bold">Total: USD {{ Cart::subtotal() }}</span></p>
 
-                    <x-button-enlace color="orange" class="w-full">
+                    <x-button-enlace href="{{ route('shopping-cart') }}" color="orange" class="w-full">
                         Ir al carrito de compras
                     </x-button-enlace>
                 </div>
