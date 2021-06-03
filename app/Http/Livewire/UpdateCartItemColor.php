0ae0ef20b1853378641f2ca184237b72d957edf7
Willy Gomez <wp_gomez@hotmail.com>
2021-06-03 17:04:52 -0500

    Video 76

--- app/Http/Livewire/UpdateCartItemColor.php
+++ app/Http/Livewire/UpdateCartItemColor.php
@@ -4,10 +4,39 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use Gloudemans\Shoppingcart\Facades\Cart;
+
 class UpdateCartItemColor extends Component
 {
-    public $rowId;
+    public $rowId, $qty;
+    public $quantity;
     
+    public function mount()
+    {
+        $item = Cart::get($this->rowId);
+        $this->qty = $item->qty;
+
+        $this->quantity = qty_available($item->id, $item->options->color_id);
+    }
+
+    public function decrement()
+    {
+        $this->qty = $this->qty - 1;
+
+        Cart::update($this->rowId, $this->qty);
+
+        $this->emitTo('dropdown-cart', 'render');
+    }
+
+    public function increment()
+    {
+        $this->qty = $this->qty + 1;
+
+        Cart::update($this->rowId, $this->qty);
+
+        $this->emitTo('dropdown-cart', 'render');
+    }
+
     public function render()
     {
         return view('livewire.update-cart-item-color');
