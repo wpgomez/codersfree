1f6e3d200b17fa764f70dc5fbf9fb7fe790c1251
Willy Gomez <wp_gomez@hotmail.com>
2021-06-09 18:38:17 -0500

    Video 113

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -1,3 +1,49 @@
 <x-app-layout>
-    
+
+    <div class="container py-12">
+
+        <div class="grid grid-cols-5 gap-6 text-white">
+            <div class="bg-red-500 bg-opacity rounded-lg px-12 pt-8 pb-4">
+                <p class="text-center text-2xl">4</p>
+                <p class="uppercase text-center">Pendiente</p>
+                <p class="text-center text-2xl mt-2">
+                    <i class="fas fa-business-time"></i>
+                </p>
+            </div>
+
+            <div class="bg-gray-500 bg-opacity rounded-lg px-12 pt-8 pb-4">
+                <p class="text-center text-2xl">4</p>
+                <p class="uppercase text-center">Recibido</p>
+                <p class="text-center text-2xl mt-2">
+                    <i class="fas fa-credit-card"></i>
+                </p>
+            </div>
+
+            <div class="bg-yellow-500 bg-opacity rounded-lg px-12 pt-8 pb-4">
+                <p class="text-center text-2xl">4</p>
+                <p class="uppercase text-center">Enviado</p>
+                <p class="text-center text-2xl mt-2">
+                    <i class="fas fa-truck"></i>
+                </p>
+            </div>
+
+            <div class="bg-pink-500 bg-opacity rounded-lg px-12 pt-8 pb-4">
+                <p class="text-center text-2xl">4</p>
+                <p class="uppercase text-center">Entregado</p>
+                <p class="text-center text-2xl mt-2">
+                    <i class="fas fa-check-circle"></i>
+                </p>
+            </div>
+
+            <div class="bg-green-500 bg-opacity rounded-lg px-12 pt-8 pb-4">
+                <p class="text-center text-2xl">4</p>
+                <p class="uppercase text-center">Anulado</p>
+                <p class="text-center text-2xl mt-2">
+                    <i class="fas fa-times-circle"></i>
+                </p>
+            </div>
+        </div>
+
+    </div>
+
 </x-app-layout>
