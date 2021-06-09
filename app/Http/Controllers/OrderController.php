0d50725c474bd4b15cbfaf4f868f4593914779df
Willy Gomez <wp_gomez@hotmail.com>
2021-06-09 15:24:31 -0500

    Video 102

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
+    public function show(Order $order)
+    {
+        return view('orders.show', compact('order'));
+    }
+
     public function payment(Order $order)
     {
         $items = json_decode($order->content);
