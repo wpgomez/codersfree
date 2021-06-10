ba5417b7663ff3c348800e26228019673e65c927
Willy Gomez <wp_gomez@hotmail.com>
2021-06-10 17:32:45 -0500

    Video 132

--- app/Http/Livewire/Admin/EditProduct.php
+++ app/Http/Livewire/Admin/EditProduct.php
@@ -2,17 +2,48 @@
 
 namespace App\Http\Livewire\Admin;
 
+use App\Models\Brand;
+use App\Models\Category;
 use App\Models\Product;
+use App\Models\Subcategory;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class EditProduct extends Component
 {
-    public $product;
+    public $product, $categories, $subcategories = [], $brands = [];
+    public $category_id = "";
+
+    protected $rules = [
+        'category_id' => 'required',
+        'product.subcategory_id' => 'required',
+        'product.name' => 'required',
+        'product.slug' => 'required|unique:products',
+        'product.description' => 'required',
+        'product.brand_id' => 'required',
+        'product.price' => 'required',
+    ];
 
     public function mount(Product $product)
     {
         $this->product = $product;
+
+        $this->categories = Category::all();
+
+        $this->category_id = $product->subcategory->category->id;
+
+        $this->subcategories = Subcategory::where('category_id', $this->category_id)->get();
+
+        $this->brands = Brand::whereHas('categories', function(Builder $query) {
+            $query->where('category_id', $this->category_id);
+        })->get();
     }
+
+    public function getSubcategoryProperty()
+    {
+        return Subcategory::find($this->product->subcategory_id);
+    }
+
     public function render()
     {
         return view('livewire.admin.edit-product')->layout('layouts.admin');
