c42a6ae41186e7d791df1df24c4c589ad60c06c1
Willy Gomez <wp_gomez@hotmail.com>
2021-06-09 19:02:00 -0500

    Video 114

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -11,7 +11,9 @@ class OrderController extends Controller
 {
     public function index()
     {
-        return view('orders.index');
+        $orders = Order::where('user_id', auth()->user()->id)->get();
+
+        return view('orders.index', compact('orders'));
     }
 
     public function show(Order $order)
