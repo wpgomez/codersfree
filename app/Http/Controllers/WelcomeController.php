ef48f205928aeb1d5b90e04128c4d04c810bf182
Willy Gomez <wp_gomez@hotmail.com>
2021-06-09 19:27:19 -0500

    Video 116

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,12 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
+        if (auth()->user()) {
+            $pendiente = Order::where('status', Order::PENDIENTE)->where('user_id', auth()->user()->id)->count();
+
+            if ($pendiente) {
+                $mensaje = "Usted tiene $pendiente ordenes pendientes. <a class='font-bold' href='" . route('orders.index') . "?status=1'>Ir a pagar</a>";
+
+                session()->flash('flash.banner', $mensaje);
+            }
+            
+        }
+        
         $categories = Category::all();
 
         return view('welcome', compact('categories'));
